7c310327604301f62b2c0b8ade19cd305414c9bf
Manoel Vieira <netinhovieira1801@gmail.com>
2023-10-10 09:18:35 -0300

    Uso do Lombok
    lombok

--- src/main/java/br/com/manoelvieira/todolist/user/UserController.java
+++ src/main/java/br/com/manoelvieira/todolist/user/UserController.java
@@ -8,10 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    
     @PostMapping("/")
     public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getName());
+        System.out.println(userModel.getUsername());
     }
-
 }
--- src/main/java/br/com/manoelvieira/todolist/user/UserModel.java
+++ src/main/java/br/com/manoelvieira/todolist/user/UserModel.java
@@ -1,31 +1,10 @@
 package br.com.manoelvieira.todolist.user;
 
+import lombok.Data; // Para colocar os Getters e Setters
+
+@Data
 public class UserModel {
     private String username;
     private String name;
     private String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
