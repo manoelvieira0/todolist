d79a78996ec8dd4698908633aa1d4ee3985db92c
Manoel Vieira <netinhovieira1801@gmail.com>
2023-10-11 15:29:12 -0300

    Criando tabela de tarefas

---
+++ src/main/java/br/com/manoelvieira/todolist/task/taskModel.java
@@ -0,0 +1,34 @@
+package br.com.manoelvieira.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data // Lombok para Getters e Setters
+@Entity(name = "tb_tasks") // Tabela
+public class taskModel {
+
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+  private String description;
+
+  @Column(length = 50) // Tamanho máximo de caracteres
+  private String title;
+  private LocalDateTime startAt;
+  private LocalDateTime endAt;
+  private String priority;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  private UUID idUser;
+
+}
