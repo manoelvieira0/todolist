92227a8c61d86a612e178243a996528ccb99f337
Manoel Vieira <netinhovieira1801@gmail.com>
2023-10-12 13:05:13 -0300

    Validando senha e usuário

--- src/main/java/br/com/manoelvieira/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/manoelvieira/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.manoelvieira.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.manoelvieira.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,34 +17,43 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component // Classe para o Spring Gerenciar
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
 
-        // Pegar a autenticação (usuário e senha)
-        var authorization = request.getHeader("Authorization"); // Recuperar autorização do Header
+    // Pegar a autenticação (usuário e senha)
+    var authorization = request.getHeader("Authorization"); // Recuperar autorização do Header
 
-        var authEncode = authorization.substring("Basic".length()).trim(); // Para separar o Basic do restante | Trim => Remover espaços que sobraram
+    var authEncode = authorization.substring("Basic".length()).trim(); // Para separar o Basic do restante | Trim =>
+                                                                       // Remover espaços que sobraram
 
-        byte[] authDecode = Base64.getDecoder().decode(authEncode); // Decodificar
+    byte[] authDecode = Base64.getDecoder().decode(authEncode); // Decodificar
 
-        var authString = new String(authDecode); // Para trazer em formato de String
+    var authString = new String(authDecode); // Para trazer em formato de String
 
-        String[] credentials = authString.split(":"); // Para separar o username do password
-        String username = credentials[0];
-        String password = credentials[1];
+    String[] credentials = authString.split(":"); // Para separar o username do password
+    String username = credentials[0];
+    String password = credentials[1];
 
-        System.out.println("Authorization");
-        System.out.println(username);
-        System.out.println(password);
+    // Validar usuário
+    var user = this.userRepository.findByUsername(username);
 
-        // Validar usuário
-
-        // Validar a senha
-
-        // Segue viagem
-    
+    if (user == null) {
+      response.sendError(401);
+    } else {
+      // Validar a senha
+      var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+      if (passwordVerify.verified) {
         filterChain.doFilter(request, response);
+      } else {
+        response.sendError(401);
+      }
+      // Segue viagem
+    }
+
   }
 
 }
