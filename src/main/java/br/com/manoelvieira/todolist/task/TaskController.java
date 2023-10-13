d81f4f10a37af4e0a91b52ce58ff79dc8c12866a
Manoel Vieira <netinhovieira1801@gmail.com>
2023-10-13 17:40:18 -0300

    Atualizando apenas tarefas do usuário que a criou

--- src/main/java/br/com/manoelvieira/todolist/task/TaskController.java
+++ src/main/java/br/com/manoelvieira/todolist/task/TaskController.java
@@ -53,11 +53,25 @@ public class TaskController {
   }
 
   @PutMapping("/{id}")
-  public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+  public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
     var task = this.taskRepository.findById(id).orElse(null);
+    
+    if(task == null){
+      ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Tarefa não encontrada");
+    }
+
+    var idUser = request.getAttribute("idUser");
+
+    if(!task.getIdUser().equals(idUser)){
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Usuário não tem permissão para alterar esta tarefa");
+    }
 
     Utils.copyNonNullProperties(taskModel, task);
 
-    return this.taskRepository.save(task);
+    var taskUpdated = this.taskRepository.save(task);
+
+    return ResponseEntity.ok().body(taskUpdated);
   }
 }
