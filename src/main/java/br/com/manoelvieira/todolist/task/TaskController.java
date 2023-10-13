2e4db7230b8e5d9d0c1193cea70c139f88f63f94
Manoel Vieira <netinhovieira1801@gmail.com>
2023-10-13 09:13:38 -0300

    verificando se a tarefa tem a data inferior que a atual

--- src/main/java/br/com/manoelvieira/todolist/task/TaskController.java
+++ src/main/java/br/com/manoelvieira/todolist/task/TaskController.java
@@ -1,8 +1,11 @@
 package br.com.manoelvieira.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +21,16 @@ public class TaskController {
   private ITaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
-    System.out.println("Chegou no controller");
+  public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
     var idUser = request.getAttribute("idUser"); // Obtendo da request
     taskModel.setIdUser((UUID) idUser); // Setando user ID
+
+    var currentDate = LocalDateTime.now();
+    if (currentDate.isAfter(taskModel.getStartAt())) { // Para verificar se a tarefa tem a data inferior que a atual
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de ínicio deve ser maior que a data atual.");
+    }
+
     var task = this.taskRepository.save(taskModel);
-    return task;
+    return ResponseEntity.status(HttpStatus.OK).body(task);
   }
 }
