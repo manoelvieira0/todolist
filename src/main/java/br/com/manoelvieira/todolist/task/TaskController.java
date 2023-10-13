781a887a85c9942e701a3918b59c31e679a30f81
Manoel Vieira <netinhovieira1801@gmail.com>
2023-10-13 09:27:51 -0300

    Listando lista de tarefas por usuário

--- src/main/java/br/com/manoelvieira/todolist/task/TaskController.java
+++ src/main/java/br/com/manoelvieira/todolist/task/TaskController.java
@@ -1,11 +1,13 @@
 package br.com.manoelvieira.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,10 +28,7 @@ public class TaskController {
     taskModel.setIdUser((UUID) idUser); // Setando user ID
 
     var currentDate = LocalDateTime.now();
-    if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) { // Para verificar se
-                                                                                                    // a tarefa tem a
-                                                                                                    // data inferior que
-                                                                                                    // a atual
+    if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) { 
       return ResponseEntity.status(HttpStatus.BAD_REQUEST)
           .body("A data de ínicio / data de término deve ser maior que a data atual.");
     }
@@ -41,5 +40,12 @@ public class TaskController {
 
     var task = this.taskRepository.save(taskModel);
     return ResponseEntity.status(HttpStatus.OK).body(task);
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request){
+    var idUser = request.getAttribute("idUser");
+    var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+    return tasks;
   }
 }
